32ab6a85002e2475709f7ecb25a4527f75700450
Diru-TOSH\Diru <chus.dirubito@gmail.com>
2016-11-18 12:23:04 +0100

    Añadir systm out

--- LectorSocket/src/lectorsocket/LectorSocket.java
+++ LectorSocket/src/lectorsocket/LectorSocket.java
@@ -16,7 +16,7 @@ import java.util.logging.Logger;
 public class LectorSocket {
 
     public static void main(String[] args) throws IOException {
-        
+        System.out.println("Hola lector");
     }
     
 }
